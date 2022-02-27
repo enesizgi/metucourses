310d72fafdf2d15381d3ad6d709853203c6bb2a9
Enes İzgi <enesizgi@tutanota.com>
2022-02-27 21:38:08 +0300

    Change github pages URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Dropdown from './components/InputBox';
-import { serverURL } from './constants';
 
 const App = () => {
   const [dropdownValue, setDropdownValue] = useState();
